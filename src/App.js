4efc43857d58b5528d5a8db2cefb2a4b99d04b66
Yashwithajuegostudio <yashwitha.juegostudio@gmail.com>
2022-05-25 17:29:09 +0530

    FEAT: destructing props implemented for functional and app class components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import "./App.css";
-import Counter from "./components/Counter";
-// import Greet from "./components/Greet";
-// import Welcome from "./components/Welcome";
-import Message from "./components/Message";
+//import Counter from "./components/Counter";
+ //import Greet from "./components/Greet";
+ import Welcome from "./components/Welcome";
+//import Message from "./components/Message";
 // import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
       {/* <Message /> */}
-      <Counter/>
+      {/* <Counter/> */}
       {/* <Greet name="Bruce" heroName="Batman">
        <p>This is a childern</p>
      </Greet>
      <Greet name="Clark" heroName="Superman">
        <button>Action</button>
      </Greet>
-     <Greet name="Diana" heroName="Wonder Woman"/>
+     <Greet name="Diana" heroName="Wonder Woman"/> */}
       <Welcome name="Bruce" heroName="Batman"></Welcome>
       <Welcome name="Clark" heroName="Superman">
        <button>Action</button>
      </Welcome>
-     <Welcome name="Diana" heroName="Wonder Woman"/> */}
-      {/* <Hello/>  */}
+     <Welcome name="Diana" heroName="Wonder Woman"/> 
+        {/* <Hello/>  */}
     </div>
   );
 }
