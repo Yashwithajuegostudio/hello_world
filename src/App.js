c14bc7ec64c5abcee046892c4646ce9ee38f29e4
Yashwithajuegostudio <yashwitha.juegostudio@gmail.com>
2022-05-25 17:18:55 +0530

    FEAT: setState Implemenation is used isnide the counter.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
- import Greet from './components/Greet';
- import Welcome  from './components/Welcome';
- import Message from './components/Message';
+import "./App.css";
+import Counter from "./components/Counter";
+// import Greet from "./components/Greet";
+// import Welcome from "./components/Welcome";
+import Message from "./components/Message";
 // import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <Message/>
-     {/* <Greet name="Bruce" heroName="Batman">
+      {/* <Message /> */}
+      <Counter/>
+      {/* <Greet name="Bruce" heroName="Batman">
        <p>This is a childern</p>
      </Greet>
      <Greet name="Clark" heroName="Superman">
@@ -20,7 +22,7 @@ function App() {
        <button>Action</button>
      </Welcome>
      <Welcome name="Diana" heroName="Wonder Woman"/> */}
-     {/* <Hello/>  */}
+      {/* <Hello/>  */}
     </div>
   );
 }
