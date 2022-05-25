4dae83e2b1e8204bb5ba0a8451ba6904f33f0d93
Yashwithajuegostudio <yashwitha.juegostudio@gmail.com>
2022-05-25 14:13:35 +0530

    FEAT:Hello.js is created to show the Implementation of JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import MyComponenets from './components/Greet';
-import Welcome  from './components/Welcome';
+// import MyComponenets from './components/Greet';
+// import Welcome  from './components/Welcome';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-     <MyComponenets/>
-     <Welcome/>
+     {/* <MyComponenets/>
+     <Welcome/> */}
+     <Hello/>
     </div>
   );
 }
